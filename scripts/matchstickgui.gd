a880ce2033a0fc04945168033a6682242eb99ecb
AvenirTheArtist <lolavenir09@gmail.com>
2026-06-06 00:10:28 +0530

    commit before sleeping 12:10am
    idk what i did i think i just added the lantern a bit

--- scripts/matchstickgui.gd
+++ scripts/matchstickgui.gd
@@ -20,7 +20,7 @@ var current_stage: stages = stages.UNLIT
 func _ready() -> void:
 	animplayer.play("matchcomingup")
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		start_burning()
 		
