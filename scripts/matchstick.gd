65e78b22f81c9ff96f1ff585ea1bf842c80399ca
not paid actor <sergejognistyj9@gmail.com>
2026-06-06 10:48:18 +0300

    map, matchsticks and gameplay

--- scripts/matchstick.gd
+++ scripts/matchstick.gd
@@ -2,4 +2,7 @@ extends Pickup
 
 ## code goes here
 func picked_up() -> void:
-	print("picked up")
+	var player = get_tree().get_first_node_in_group("player")
+	player.matchstick_amount += 1
+	print("picked up match")
+	queue_free()
