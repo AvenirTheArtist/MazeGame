3213c40ed2cc90ee9c7966cbbaec559f08a791a8
not paid actor <sergejognistyj9@gmail.com>
2026-06-09 14:47:09 +0300

    rebalance + los detection

--- scripts/ghost.gd
+++ scripts/ghost.gd
@@ -7,16 +7,20 @@ enum states {
 	CHASING,
 	STUNNED
 }
-@export var speed = 9.0
+@export var chase_speed: float
+@export var stun_speed: float
+@export var alert_speed: float
 @export var patrol_pos: Array[Vector3]
 var alerted_pos: Vector3
 var next_dest: Vector3
 
 var state = states.ROAMING
+var player_in_range = false
 var update_timer = 0.0
 var stun_immunity = 0.0
 
 @onready var player_proximity = $player_proximity_detection
+@onready var los_ray = $los_detection
 @onready var nav = $NavigationAgent3D
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var sound_player: Node3D = $sound_player
@@ -37,7 +41,18 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if !all_sounds["proximity_sound"].playing:
 		all_sounds["proximity_sound"].play()
-
+	
+	#region LOS
+	## TEST /// LOS detection code ///
+	if player_in_range:
+		los_ray.look_at(player.global_position)
+		if los_ray.is_colliding():
+			if los_ray.get_collider() is Player and state != states.CHASING:
+				state = states.CHASING
+				all_sounds["alerted_scream"].play()
+				#print("found")
+	#endregion
+	
 	#TEMPORARY
 	if Input.is_action_just_pressed("ui_accept"):
 		move_to_player()
@@ -46,8 +61,11 @@ func _physics_process(delta: float) -> void:
 	if stun_immunity > 0:
 		stun_immunity -= delta
 	
+	var speed_final: float
 	#region /// updating navigation 
 	match state:
+		states.ALERTED:
+			pass
 		states.CHASING:
 			update_timer -= delta
 			if update_timer <= 0:
@@ -55,9 +73,8 @@ func _physics_process(delta: float) -> void:
 				var rand = randf_range(-0.1, 0.1)
 				randomize()
 				update_timer += 0.2 + rand
-			
-		#states.STUNNED:
-			#next_dest = self.global_position
+		states.STUNNED:
+			speed_final = stun_speed
 	#endregion
 	
 	nav.target_position = next_dest
@@ -66,7 +83,7 @@ func _physics_process(delta: float) -> void:
 	direction.x = next_pos.x - global_position.x
 	direction.z = next_pos.z - global_position.z
 	
-	var vel = direction.normalized() * speed
+	var vel = direction.normalized() * speed_final
 	velocity = vel
 	
 	move_and_slide()
@@ -82,20 +99,20 @@ func update_roaming() -> Vector3:
 
 ## move to a collected bell
 func move_to_bell(pos) -> void:
-	if state != states.CHASING:
+	if state != states.CHASING and state != states.STUNNED:
 		alerted_pos = pos
 		state = states.ALERTED
 		all_sounds["alerted_scream"].play()
 		next_dest = alerted_pos
 
 func move_to_player():
-	if state != states.CHASING:
+	if state != states.CHASING and state != states.STUNNED:
 		alerted_pos = player.global_position
 		state = states.ALERTED
 		all_sounds["alerted_scream"].play()
 		next_dest = alerted_pos
 
-## TEST TEST TEST 5 seconds of stun is ok? yes for now, might change later
+## TEST TEST TEST changed stun duration to 9s
 func get_stunned() -> void:
 	if stun_immunity > 0: return
 	player_proximity.get_child(0).disabled = true
@@ -108,19 +125,20 @@ func get_stunned() -> void:
 		if dist > 10 and dist < 25:
 			defined_pos = i
 	next_dest = defined_pos
-	stun_immunity = 15.0
+	stun_immunity = 18.0
 	await get_tree().create_timer(
-		5.0, false
+		9.0, false
 	).timeout
 	state = states.ROAMING
 	player_proximity.get_child(0).disabled = false
 	
 
 ## start chase when player_proximity_detection sees a player
+
+
 func _on_player_proximity_detection_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
-		state = states.CHASING
-		all_sounds["alerted_scream"].play()
+		player_in_range = true
 
 
 func _on_player_proximity_detection_body_exited(body: Node3D) -> void:
