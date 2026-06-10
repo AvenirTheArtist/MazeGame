33fdcb5df0b11a3930c2553e2262cd41a1cfa7d7
AvenirTheArtist <lolavenir09@gmail.com>
2026-06-11 00:16:47 +0530

    bell ambience
    and some other visual changes

--- scripts/pickup_test.gd
+++ scripts/pickup_test.gd
@@ -5,10 +5,16 @@ var activated = false
 func _ready() -> void:
 	get_parent().number_of_bells += 1
 
+func _process(delta: float) -> void:
+	if not activated:
+		if !$ambience.playing:
+			$ambience.play()
+
 func picked_up() -> void:
 	if not activated:
 		get_parent().bells_collected += 1
-		$AudioStreamPlayer3D.play()
+		$bellring.play()
+		$ambience.stop()
 		$bell2.get_node("AnimationPlayer").play("swinging")
 		get_parent().bell_collected.emit(self.global_position)
 		activated = true
