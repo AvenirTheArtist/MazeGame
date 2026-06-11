cb3bf38e2ec32b56fce920d4ce6069a0f716f27d
AvenirTheArtist <lolavenir09@gmail.com>
2026-06-12 02:01:11 +0530

    commit for elwario

--- scripts/game_data.gd
+++ scripts/game_data.gd
@@ -5,3 +5,7 @@ signal bell_collected(pickup_pos)
 
 @export var number_of_bells: int
 var bells_collected = 0
+
+func _process(delta: float) -> void:
+	if bells_collected >= 7:
+		get_tree().change_scene_to_file("res://youwin.tscn")
